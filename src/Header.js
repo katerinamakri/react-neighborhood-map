587bb343db7e4f0b01b25c75548d25f2c48bf618
katerinamakri <kathrin023@gmail.com>
2018-08-07 17:00:18 +0300

    Make header a new component

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Header extends Component {
+
+	render() {
+		return (
+			<div className="App-header">
+				<h1 className="App-title">Co-working Spaces in Athens</h1>
+				<button>Close</button>				
+			</div>
+		)
+
+	}
+}
+
+export default Header
