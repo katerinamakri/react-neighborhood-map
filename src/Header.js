0951a7b4777d21490771f92d5e547c56bfd2410c
katerinamakri <kathrin023@gmail.com>
2018-08-09 23:02:12 +0300

    Created functionality of hamburger icon

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,28 @@ import './App.css';
 
 class Header extends Component {
 
+	//handle hamburger icon
+	toggleSiderMenu (){
+	  console.log(this.props.isSiderMenuOpen)
+	  if (this.props.isSiderMenuOpen) {
+	    document.getElementById("sidermenu").style.display = "none";
+	    this.props.setState({isSiderMenuOpen: false}) // error:de mporei na allaxei to state tou parent
+	  } else {
+	    document.getElementById("sidermenu").style.display = "block";
+	    this.props.setState({isSiderMenuOpen: true})
+	  }   
+	} 
+
 	render() {
+
+		// let displaySiderMenu = this.props.isSiderMenuOpen ? "block" : "none";
 		return (
-			<div className="App-header">
-				<h1 className="App-title">Co-working Spaces in Athens</h1>
-				<button>Close</button>				
-			</div>
+	        <div className="App-header">
+	          <button onClick={() => this.toggleSiderMenu()}> 
+	          	<i className="fas fa-bars"></i> 
+	          </button>       
+	          <h1 className="App-title">Co-working Spaces in Athens</h1>
+	        </div>
 		)
 
 	}
