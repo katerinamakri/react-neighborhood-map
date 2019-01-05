e958ca78646772dcfafd817710749124b2363cfd
katerinamakri <kathrin023@gmail.com>
2019-01-05 16:51:42 +0200

    Updates

---
+++ precache-manifest.8c29874dd9dbd30fc3d77c4246e0224f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df449222aabb8d7a6ef7",
+    "url": "/react-neighborhood-map/static/js/runtime~main.df449222.js"
+  },
+  {
+    "revision": "f51d4db72a88f5e4b0f6",
+    "url": "/react-neighborhood-map/static/js/main.f51d4db7.chunk.js"
+  },
+  {
+    "revision": "84e6e48d29d0bb9e8cb6",
+    "url": "/react-neighborhood-map/static/js/1.84e6e48d.chunk.js"
+  },
+  {
+    "revision": "f51d4db72a88f5e4b0f6",
+    "url": "/react-neighborhood-map/static/css/main.0a0a640d.chunk.css"
+  },
+  {
+    "revision": "b4fa65d3b333993239d117770962dbb1",
+    "url": "/react-neighborhood-map/index.html"
+  }
+];
